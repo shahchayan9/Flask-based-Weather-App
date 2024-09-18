61e248295a151bef8cbd1ab9d5ef2c8b8350af01
shah-chayan9 <chayan.shah@cogoport.com>
2024-09-18 09:17:56 -0700

    Added another unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,3 +9,13 @@ def client():
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
+
+def test_weather(client):
+    response = client.get('/weather?city=London')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert 'city' in data
+    assert 'current_time' in data
+    assert 'description' in data
+    assert 'icon' in data
+    assert 'temperature' in data
